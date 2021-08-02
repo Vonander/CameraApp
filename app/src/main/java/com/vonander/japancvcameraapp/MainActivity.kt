115df56e9a1490073fc931f3f7e1de36b0fa657d
Johan Fornander <johan.fornander@outlook.com>
2021-08-02 19:26:11 +0200

    initial commit

---
+++ app/src/main/java/com/vonander/japancvcameraapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.vonander.japancvcameraapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.vonander.japancvcameraapp.ui.theme.JapanCVCameraAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JapanCVCameraAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JapanCVCameraAppTheme {
+        Greeting("Android")
+    }
+}
